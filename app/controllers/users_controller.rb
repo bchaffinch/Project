97ac79abeeb0de8abac01ea64ffdc8bdfbf4ced7
Brian Chaffinch <bchaffinch@gmail.com>
2015-03-09 13:39:04 -0400

    Homework 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save!
+  		#Notifications.new_user(@user)
   		redirect_to users_path
   	else
   		render 'new'
@@ -20,9 +21,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def show
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(user_params)
+  		redirect_to user_path(@user.id)
+  	else
+  		render 'edit'
+  		#will return them to the edit page so they can try again#
+ 	end
+ end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to user_path
   end
 
   private
@@ -32,3 +51,8 @@ class UsersController < ApplicationController
   end
 
 end
+
+#<p><%= image_tag @user.avatar.url(:medium) %> 
+#<br>
+#	<p><%= image_tag @user.avatar.url(:thumb) %> 
+#	<br>
